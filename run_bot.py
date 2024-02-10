922f3935cda37d92c26bc02efe6e2d1a78e98108
Doson <karbushev-1991@mail.ru>
2024-02-10 08:37:41 +0700

    добавление habr freelance

--- run_bot.py
+++ run_bot.py
@@ -1,13 +1,15 @@
 import time
 
 from data.kwork import main as run_kwork
+from data.habr_freelance import main as run_habr
 from bot.main import run_telegram_wrapper as run_telegram
 
 
 def main():
     while True:
         run_kwork()
-        time.sleep(60 * 5)
+        run_habr()
+        time.sleep(60 * 1)
 
 
 if __name__ == "__main__":
