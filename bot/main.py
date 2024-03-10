fb0c196b6be59181369cb14a154ffb9dc7e02cd0
Doson <karbushev-1991@mail.ru>
2024-03-10 20:49:07 +0700

    установил часовой пояс красноярск

--- bot/main.py
+++ bot/main.py
@@ -11,6 +11,8 @@ sys.path.append(str(Path.cwd().parent))
 
 from data.database import SQLite_operations
 import configparser
+from datetime import datetime
+import pytz
 
 # Достать API_TOKEN из config.ini
 config = configparser.ConfigParser()
@@ -22,12 +24,13 @@ config.read(dir_conf)
 API_TOKEN = config['telegram']['API_TOKEN']
 CHAT_ID = config['telegram']['CHAT_ID']
 
+krasnoyarsk_tz = pytz.timezone('Asia/Krasnoyarsk')
 # Создаем экземпляр бота
 bot = Bot(token=API_TOKEN)
 
 
 def get_messages(start_time):
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_time = datetime.now(krasnoyarsk_tz).strftime("%Y-%m-%d %H:%M:%S")
     logger.info(f"Запрос сообщений с {start_time} по {current_time}")
 
     # Создаем объект Path для текущего каталога
@@ -68,11 +71,12 @@ async def send_messages_to_chat(message):
 
 async def main():
     # start_time = '2024-02-11 18:14:50'
-    start_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    start_time = datetime.now(krasnoyarsk_tz).strftime("%Y-%m-%d %H:%M:%S")
     while True:
         messages = get_messages(start_time)
         if messages:
-            start_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            start_time = datetime.now(krasnoyarsk_tz).strftime("%Y-%m-%d %H:%M:%S")
         for message in messages:
             format_message = format_order_message(*message)
             await send_messages_to_chat(format_message)
