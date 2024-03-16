8c03411875802603d7ad4f0a742afdea2b005e40
Doson <karbushev-1991@mail.ru>
2024-03-16 13:02:46 +0700

    вкл хабр

--- run_bot.py
+++ run_bot.py
@@ -8,7 +8,7 @@ from data.FLru import main as run_flru
 def main():
     while True:
         # run_kwork()
-        # run_habr()
+        run_habr()
         run_flru()
 
         time.sleep(60 * 5)
